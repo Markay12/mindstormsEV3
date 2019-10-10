c090159165b48dd1e09f0c0f431abe5f72fc75c3
Markay12 <54555076+Markay12@users.noreply.github.com>
2019-10-10 14:37:17 -0700

    Tests two different motors

--- motor/TwoMotors.m
+++ motor/TwoMotors.m
@@ -1,10 +1,10 @@
 brick.MoveMotor('A', -100);
 brick.MoveMotor('B', 100);
 pause(1)
-brick.MoveMotor('A', 50);
+brick.MoveMotor('A', 200);
 brick.StopMotor('B');
 pause(3)
-brick.MoveMotor('B', -50)
+brick.MoveMotor('B', -200)
 pause(5)
 brick.StopMotor('A')
 brick.StopMotor('B')
