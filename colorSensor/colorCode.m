b76a29e3f59dd412c4a16d1e49b4bfd7a5174eee
Markay12 <54555076+Markay12@users.noreply.github.com>
2019-10-17 13:40:07 -0700

    Test the color sensor with color code

---
+++ colorSensor/colorCode.m
@@ -0,0 +1,16 @@
+%% Set colorSensor to colorCode
+
+brick.SetColorMode(4, 2); % set colorCode
+
+color = brick.ColorCode(4); % displays the color
+
+disp(color);
+
+% NaC = 0
+% black = 1
+% blue = 2
+% green = 3
+% yellow = 4
+% red = 5
+% white = 6
+% brown = 7
