fbca713c9622b551dcfca0c08ec9ff92d87f27b5
Markay12 <54555076+Markay12@users.noreply.github.com>
2019-11-14 12:36:58 -0700

    Autonomous Control of the EV3 vehicle

--- automation/firstControl.m
+++ automation/firstControl.m
@@ -17,17 +17,35 @@ while 1
                 distance = brick.UltrasonicDist(3);
                 color = brick.LightAmbient(4);
                 
-                brick.MoveMotor('A', 50);
-                brick.MoveMotor('B', 50);
                 
-                if (distance < 20 && color ~= 5)
+                if (distance < 20)
+                    pause(0.85);
                     brick.StopMotor('A');
                     brick.StopMotor('B');
-                    brick.MoveMotor('A', 60);
-                    brick.MoveMotor('B', -60);
-                
+                    brick.MoveMotor('A', 20);
+                    brick.MoveMotor('B', -20);
+                    pause(1.5);
+                    brick.StopMotor('A');
+                    brick.StopMotor('B');
+                    brick.MoveMotor('A', 40);
+                    brick.MoveMotor('B', -40);
+                    pause(2);
+                    
+                elseif (distance < 5)
+                    brick.StopMotor('A');
+                    brick.StopMotor('B');
+                    pause(0.5);
+                    brick.MoveMotor('A', 50);
+                    brick.MoveMotor('B', -50);
+                    pause(0.25);
+                else
+                    brick.MoveMotor('A', 40);
+                    brick.MoveMotor('B', 40);
                 end
             end
+            
+            
+            
             
             
         case 'q'
