968e28ae29ed163ba87e4d7b1264b4d4bda97928
Markay12 <54555076+Markay12@users.noreply.github.com>
2019-11-14 14:30:49 -0700

    Main autonomous control

--- automation/firstControl.m
+++ automation/firstControl.m
@@ -4,6 +4,9 @@ InitKeyboard();
 brick.SetColorMode(4, 1); % set the color mode of the robot
 
 g = 0;
+d = 0;
+f = 0;
+h = 0;
 
 while 1
     pause(0.01);
@@ -17,32 +20,29 @@ while 1
                 distance = brick.UltrasonicDist(3);
                 color = brick.LightAmbient(4);
                 
+                brick.MoveMotor('A', 50);
+                brick.MoveMotor('B', 47);
                 
-                if (distance < 20)
-                    pause(0.85);
-                    brick.StopMotor('A');
-                    brick.StopMotor('B');
-                    brick.MoveMotor('A', 20);
-                    brick.MoveMotor('B', -20);
-                    pause(1.5);
-                    brick.StopMotor('A');
-                    brick.StopMotor('B');
-                    brick.MoveMotor('A', 40);
-                    brick.MoveMotor('B', -40);
-                    pause(2);
-                    
-                elseif (distance < 5)
-                    brick.StopMotor('A');
-                    brick.StopMotor('B');
-                    pause(0.5);
+                
+                
+                if distance < 20
                     brick.MoveMotor('A', 50);
                     brick.MoveMotor('B', -50);
-                    pause(0.25);
-                else
-                    brick.MoveMotor('A', 40);
-                    brick.MoveMotor('B', 40);
+                    pause(0.5);
+                    brick.StopMotor('A');
+                    brick.StopMotor('B');
+                    pause(1);
                 end
+                
+                brick.MoveMotor('A', 50);
+                brick.MoveMotor('B', 47);
+                
+                
             end
+            
+            
+            
+            
             
             
             
@@ -54,6 +54,7 @@ while 1
             brick.StopMotor('A');
             brick.StopMotor('B');
             break;
+            
     end
 end
 
