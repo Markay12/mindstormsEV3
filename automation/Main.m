b9645675008cef759629699a23f6eab64842d04f
Markay12 <54555076+Markay12@users.noreply.github.com>
2019-11-25 14:26:03 -0700

    main code, used to control the robot

--- automation/Main.m
+++ automation/Main.m
@@ -1,322 +1,107 @@
 global key
 InitKeyboard();
 
-while 2
-    color = brick.ColorColor(1);
+startMoving = 0;
+
+numRightTurns = 0;
+numLeftTurns = 0;
+
+while 1
+    pause(0.1);
+    distance = brick.UltrasonicDist(3);
     
-    switch color
-        case 2  % blue
-            while 1
-                pause (0.1);
-                switch key
-                    case 'uparrow'
-                        disp('up Arrow Pressed');
-                        brick.MoveMotor('A',50);
-                        brick.MoveMotor('B',50);
-                        
-                    case 'downarrow'
-                        disp('down Arrow Pressed');
-                        brick.MoveMotor('A',-50);
-                        brick.MoveMotor('B',-50);
-                        
-                    case 'leftarrow'
-                        disp('left Arrow Pressed');
-                        brick.MoveMotor('A',50);
-                        brick.MoveMotor('B',0);
-                        
-                    case 'rightarrow'
-                        disp('right Arrow Pressed');
-                        brick.MoveMotor('A',0);
-                        brick.MoveMotor('B',50);
-                        
-                    case 'd'
-                        disp('Gard rail down');
-                        brick.MoveMotor('C', -25);
-                        
-                    case 'f'
-                        disp('Gard rail down');
-                        brick.MoveMotor('C', 25);
-                        
-                    case 0
-                        disp('No key presed');
-                        brick.StopMotor('A');
-                        brick.StopMotor('B');
-                        brick.StopMotor('C');
-                    case 'b'
-                        color = brick.ColorColor(1);
-                        disp (color);
-                    case 'q'
-                        brick.StopMotor('A');
-                        brick.StopMotor('B');
-                        break;
-                        
-                        
+    switch key
+        
+        case 'uparrow' % on the up arrow, the auto-driving will begin
+            
+            while(startMoving == 0)
+                
+                distance = brick.UltrasonicDist(3);
+                
+                if (distance > 20)
+                    distance = brick.UltrasonicDist(3);
+                    
+                    brick.MoveMotor('A', 50);
+                    brick.MoveMotor('B', 47);
+                    numRightTurns = 0;
+                    numLeftTurns = 0;
+                    
+                    
+                elseif (distance < 20)
+                    
+                    
+                    % rotate right
+                    % 90 degree left turn
+                    brick.MoveMotor('A', -27);
+                    brick.MoveMotor('B', 27);
+                    
+                    pause(0.875);
+                    
+                    brick.MoveMotor('A', 50);
+                    brick.MoveMotor('B', 47);
+                    
+                    pause(1);
+                    
+                    numRightTurns = numRightTurns + 1;
+                    
+                    if (numRightTurns == 3)
+                        numRightTurns = 0;
+                        numLeftTurns = 0;
+                    end
+                    
+                    distance = brick.UltrasonicDist(3);
+                    
+                elseif (distance > 18.3)
+                    
+                    distance = brick.UltrasonicDist(3);
+                    
+                    % 180 degree left turn
+                    
+                    brick.MoveMotor('A', 44.95);
+                    brick.MoveMotor('B', -44.95);
+                    
+                    pause(0.875);
+                    
+                    brick.MoveMotor('A', 50);
+                    brick.MoveMotor('B', 47);
+                    
+                    pause(2);
+                    
+                    numLeftTurns = numLeftTurns + 1;
+                    
+                    if(numLeftTurns == 3)
+                        numLeftTurns = 0 ;
+                    end
+                    
+                    brick.UltrasonicDist(3);
+                    
+                else
+                    
+                    % move motors forward
+                    brick.MoveMotor('A', 50);
+                    brick.MoveMotor('B', 47);
+                    
+                    pause(0.1);
+                    
+                    numLeftTurns = 0;
+                    numRightTurns = 0;
+                    
                 end
+                
             end
             
-        case 4  % yellow
-            while 1
-                pause (0.1);
-                switch key
-                    case 'uparrow'
-                        disp('up Arrow Pressed');
-                        brick.MoveMotor('A',50);
-                        brick.MoveMotor('B',50);
-                        
-                    case 'downarrow'
-                        disp('down Arrow Pressed');
-                        brick.MoveMotor('A',-50);
-                        brick.MoveMotor('B',-50);
-                        
-                    case 'leftarrow'
-                        disp('left Arrow Pressed');
-                        brick.MoveMotor('A',50);
-                        brick.MoveMotor('B',0);
-                        
-                    case 'rightarrow'
-                        disp('right Arrow Pressed');
-                        brick.MoveMotor('A',0);
-                        brick.MoveMotor('B',50);
-                        
-                    case 'd'
-                        disp('Gard rail down');
-                        brick.MoveMotor('C', -25);
-                        
-                    case 'f'
-                        disp('Gard rail down');
-                        brick.MoveMotor('C', 25);
-                        
-                    case 0
-                        disp('No key presed');
-                        brick.StopMotor('A');
-                        brick.StopMotor('B');
-                        brick.StopMotor('C');
-                    case 'b'
-                        color = brick.ColorColor(1);
-                        disp (color);
-                    case 'q'
-                        brick.StopMotor('A');
-                        brick.StopMotor('B');
-                        break;
-                        
-                        
-                end
-            end
             
-        case 5  % red
+            
+        case 'q'
+            
+            disp('Quit Program');
             brick.StopMotor('A');
             brick.StopMotor('B');
+            break;
             
-            %
-            %
-            %
-            %
-            %
-            %
-            %
-            %
-        case 1  % black
-            color = brick.ColorColor(1);
-            disp (color);
-            distance = brick.UltrasonicDist(2);
-            if distance < 45 %if distance less than 45, brick will go stright
-                brick.MoveMotor('A',25.5);
-                brick.MoveMotor('B',26);
-                if brick.TouchPressed(4) %if touched, turn 90 degrees
-                    
-                    brick.StopMotor('A');
-                    brick.StopMotor('B');
-                    color = brick.ColorColor(1);
-                    disp (color);
-                    pause(1);
-                    brick.MoveMotor('A',-24.5);
-                    brick.MoveMotor('B',-26);
-                    pause(2);
-                    angle = brick.GyroAngle(3);
-                    needed = -83.5 + angle;
-                    while angle > needed
-                        brick.MoveMotor('A',25);
-                        brick.MoveMotor('B',0);
-                        angle = brick.GyroAngle(3);
-                    end
-                    brick.MoveMotor('A',25.25);
-                    brick.MoveMotor('B',24);
-                    pause(2.5);
-                    distance = brick.UltrasonicDist(2);
-                    
-                end
-            else
-                brick.MoveMotor('A',25.25);
-                brick.MoveMotor('B',24);
-                pause (1);
-                brick.StopMotor('A');
-                brick.StopMotor('B');
-                angle = brick.GyroAngle(3);
-                needed = 82.5 + angle;
-                while angle < needed
-                    brick.MoveMotor('A',0);
-                    brick.MoveMotor('B',25);
-                    angle = brick.GyroAngle(3);
-                end
-                brick.MoveMotor('A',25.25);
-                brick.MoveMotor('B',24);
-                pause(3);
-                distance = brick.UltrasonicDist(2);
-            end
-            
-        case 0 % unknown color
-            color = brick.ColorColor(1);
-            disp (color);
-            distance = brick.UltrasonicDist(2);
-            if distance < 45 %if distance less than 45, brick will go stright
-                brick.MoveMotor('A',25.5);
-                brick.MoveMotor('B',26);
-                if brick.TouchPressed(4) %if touched, turn 90 degrees
-                    
-                    brick.StopMotor('A');
-                    brick.StopMotor('B');
-                    color = brick.ColorColor(1);
-                    disp (color);
-                    pause(1);
-                    brick.MoveMotor('A',-24.5);
-                    brick.MoveMotor('B',-26);
-                    pause(2);
-                    angle = brick.GyroAngle(3);
-                    needed = -83.5 + angle;
-                    while angle > needed
-                        brick.MoveMotor('A',25);
-                        brick.MoveMotor('B',0);
-                        angle = brick.GyroAngle(3);
-                    end
-                    brick.MoveMotor('A',25.25);
-                    brick.MoveMotor('B',24);
-                    pause(2.5);
-                    distance = brick.UltrasonicDist(2);
-                    
-                end
-            else
-                brick.MoveMotor('A',25.25);
-                brick.MoveMotor('B',24);
-                pause (1);
-                brick.StopMotor('A');
-                brick.StopMotor('B');
-                angle = brick.GyroAngle(3);
-                needed = 82.5 + angle;
-                while angle < needed
-                    brick.MoveMotor('A',0);
-                    brick.MoveMotor('B',25);
-                    angle = brick.GyroAngle(3);
-                end
-                brick.MoveMotor('A',25.25);
-                brick.MoveMotor('B',24);
-                pause(3);
-                
-                distance = brick.UltrasonicDist(2);
-            end
-            
-            %end
-            
-        case 6  % white
-            color = brick.ColorColor(1);
-            disp (color);
-            distance = brick.UltrasonicDist(2);
-            if distance < 45 %if distance less than 45, brick will go stright
-                brick.MoveMotor('A',25.5);
-                brick.MoveMotor('B',26);
-                if brick.TouchPressed(4) %if touched, turn 90 degrees
-                    
-                    brick.StopMotor('A');
-                    brick.StopMotor('B');
-                    color = brick.ColorColor(1);
-                    disp (color);
-                    pause(1);
-                    brick.MoveMotor('A',-24.5);
-                    brick.MoveMotor('B',-26);
-                    pause(2);
-                    angle = brick.GyroAngle(3);
-                    needed = -83.5 + angle;
-                    while angle > needed
-                        brick.MoveMotor('A',25);
-                        brick.MoveMotor('B',0);
-                        angle = brick.GyroAngle(3);
-                    end
-                    brick.MoveMotor('A',25.25);
-                    brick.MoveMotor('B',24);
-                    pause(2.5);
-                    distance = brick.UltrasonicDist(2);
-                    
-                end
-            else
-                brick.MoveMotor('A',25.25);
-                brick.MoveMotor('B',24);
-                pause (1);
-                brick.StopMotor('A');
-                brick.StopMotor('B');
-                angle = brick.GyroAngle(3);
-                needed = 82.5 + angle;
-                while angle < needed
-                    brick.MoveMotor('A',0);
-                    brick.MoveMotor('B',25);
-                    angle = brick.GyroAngle(3);
-                end
-                brick.MoveMotor('A',25.25);
-                brick.MoveMotor('B',24);
-                pause(3);
-                
-                distance = brick.UltrasonicDist(2);
-            end
-            
-            %end
-            
-        case 7  % brown
-            color = brick.ColorColor(1);
-            disp (color);
-            distance = brick.UltrasonicDist(2);
-            if distance < 45 %if distance less than 45, brick will go stright
-                brick.MoveMotor('A',25.5);
-                brick.MoveMotor('B',26);
-                if brick.TouchPressed(4) %if touched, turn 90 degrees
-                    
-                    brick.StopMotor('A');
-                    brick.StopMotor('B');
-                    color = brick.ColorColor(1);
-                    disp (color);
-                    pause(1);
-                    brick.MoveMotor('A',-24.5);
-                    brick.MoveMotor('B',-26);
-                    pause(2);
-                    angle = brick.GyroAngle(3);
-                    needed = -83.5 + angle;
-                    while angle > needed
-                        brick.MoveMotor('A',25);
-                        brick.MoveMotor('B',0);
-                        angle = brick.GyroAngle(3);
-                    end
-                    brick.MoveMotor('A',25.25);
-                    brick.MoveMotor('B',24);
-                    pause(2.5);
-                    distance = brick.UltrasonicDist(2);
-                    
-                end
-            else
-                brick.MoveMotor('A',25.25);
-                brick.MoveMotor('B',24);
-                pause (1);
-                brick.StopMotor('A');
-                brick.StopMotor('B');
-                angle = brick.GyroAngle(3);
-                needed = 82.5 + angle;
-                while angle < needed
-                    brick.MoveMotor('A',0);
-                    brick.MoveMotor('B',25);
-                    angle = brick.GyroAngle(3);
-                end
-                brick.MoveMotor('A',25.25);
-                brick.MoveMotor('B',24);
-                pause(3);
-                
-                distance = brick.UltrasonicDist(2);
-            end
-            
-    end
-end
+    end % end of switch statement
+end % end of while loop
+
+
+CloseKeyboard();
+
