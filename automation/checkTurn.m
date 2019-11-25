a613a5b641b2134f6d7a102234beb20af3ffbb64
Markay12 <54555076+Markay12@users.noreply.github.com>
2019-11-25 14:26:27 -0700

    Used to create functions for turning

---
+++ automation/checkTurn.m
@@ -0,0 +1,51 @@
+global key
+InitKeyboard();
+
+while 1
+    pause(0.1);
+    
+    
+    switch key
+        
+        case 'uparrow'
+            
+            % 180 degree turn
+            
+            brick.MoveMotor('A', 44.95);
+            brick.MoveMotor('B', -44.95);
+            pause(0.875);
+            brick.StopMotor('A');
+            brick.StopMotor('B');
+            
+            
+        case 'downarrow'
+            
+            % 90 degree left turn
+            brick.MoveMotor('A', 27);
+            brick.MoveMotor('B', -27);
+            pause(0.875);
+            brick.StopMotor('A');
+            brick.StopMotor('B');
+            
+            
+        case 'rightarrow'
+            
+            % 180 degree right
+            
+            brick.MoveMotor('A', -49.1);
+            brick.MoveMotor('B', 49.1);
+            pause(0.875);
+            brick.StopMotor('A');
+            brick.StopMotor('B');
+            
+            
+        case 'q'
+            
+            brick.StopMotor('A');
+            brick.StopMotor('B');
+            break;
+            
+    end
+end
+
+CloseKeyboard();
