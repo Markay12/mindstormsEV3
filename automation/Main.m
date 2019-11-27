7926a4039fea28d0817032113cd899e620d4514c
Markay12 <54555076+Markay12@users.noreply.github.com>
2019-11-27 07:32:12 -0700

    Made sure the robot could keep moving straight

--- automation/Main.m
+++ automation/Main.m
@@ -21,59 +21,66 @@ while 1
                 if (distance > 15)
                     
                     brick.MoveMotor('A', 50);
-                    brick.MoveMotor('B', 47);
+                    brick.MoveMotor('B', 48.75);
                     numRightTurns = 0;
                     numLeftTurns = 0;
                     
                     distance = brick.UltrasonicDist(3);
+                    disp(distance);
                     
                 elseif (distance < 15)
                     
-                    % turn left
+                    % turn right
                     
-                    brick.MoveMotor('A', 27);
-                    brick.MoveMotor('B', -27);
+                    brick.MoveMotor('A', -29);
+                    brick.MoveMotor('B', 27);
                     pause(0.875);
                     brick.StopMotor('A');
                     brick.StopMotor('B');
-                    numLeftTurns = 1;
-                    
+                    numRightTurns = 1;
+                    pause(0.5);
                     distance = brick.UltrasonicDist(3);
                     disp(distance);
+                    pause(0.5);
+
                     
                     if (distance < 15)
                         
                         % turn to the right
                         
                         brick.MoveMotor('A', -49.1);
-                        brick.MoveMotor('B', 49.1);
+                        brick.MoveMotor('B', 45.9125);
                         pause(0.875);
                         brick.StopMotor('A');
                         brick.StopMotor('B');
                         
                         distance = brick.UltrasonicDist(3);
                         
+
+                        
                         if (distance < 15)
                             
                             % turn backwards (around 3pi/2)
                             
-                            brick.MoveMotor('A', -27);
-                            brick.MoveMotor('B', 27);
+                            brick.MoveMotor('A', 27);
+                            brick.MoveMotor('B', -29);
                             pause(0.875);
                             brick.StopMotor('A');
                             brick.StopMotor('B');
-                            startMoving = 1;
+                            startMoving = 0;
+                            
+                            distance = brick.UltrasonicDist(3);
                             
                             
                         end
                         
+                        
+                        
+                        
+                        
                     end
-                    
-                    
-                    
                 end
             end
-            
             
             
             
