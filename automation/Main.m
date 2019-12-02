c2dd2695f297d70946424f473fca072c9b1d6e20
Markay12 <54555076+Markay12@users.noreply.github.com>
2019-12-01 18:03:19 -0700

    edit to autonomous driving, check README.md December update

--- automation/Main.m
+++ automation/Main.m
@@ -1,6 +1,8 @@
 global key
 InitKeyboard();
 
+brick.SetColorMode(4, 2); % set sensor 4 to colorCode 
+
 startMoving = 0;
 
 numRightTurns = 0;
@@ -8,7 +10,8 @@ numLeftTurns = 0;
 
 while 1
     pause(0.1);
-    distance = brick.UltrasonicDist(3);
+    distance = brick.UltrasonicDist(3); % gather the distance data
+    color = brick.ColorCode(4); % gather the color data
     
     switch key
         
@@ -17,22 +20,35 @@ while 1
             while(startMoving == 0)
                 
                 distance = brick.UltrasonicDist(3);
+                color = brick.ColorCode(4);
                 
-                if (distance > 15)
+                
+                
+                if (distance > 15 && color ~= 5)
                     
                     brick.MoveMotor('A', 50);
-                    brick.MoveMotor('B', 48.75);
+                    brick.MoveMotor('B', 48.6465);
                     numRightTurns = 0;
                     numLeftTurns = 0;
                     
                     distance = brick.UltrasonicDist(3);
                     disp(distance);
                     
+                
+                    
+                elseif (distance > 15 && color == 5)
+                    brick.StopMotor('A');
+                    brick.StopMotor('B');
+                    pause(3);
+                    brick.MoveMotor('A', 50);
+                    brick.MoveMotor('B', 48.6465);
+                    pause(1);
+                    
                 elseif (distance < 15)
                     
                     % turn right
                     
-                    brick.MoveMotor('A', -29);
+                    brick.MoveMotor('A', -28.785);
                     brick.MoveMotor('B', 27);
                     pause(0.875);
                     brick.StopMotor('A');
@@ -42,34 +58,38 @@ while 1
                     distance = brick.UltrasonicDist(3);
                     disp(distance);
                     pause(0.5);
+                    
+                    distance = brick.UltrasonicDist(3);
+                    disp(distance);
 
                     
                     if (distance < 15)
                         
-                        % turn to the right
+                        % turn to the left 180 degrees
                         
-                        brick.MoveMotor('A', -49.1);
-                        brick.MoveMotor('B', 45.9125);
+                        brick.MoveMotor('A',  45.9125); % -49.1
+                        brick.MoveMotor('B', -49.1); % 45.9125
                         pause(0.875);
                         brick.StopMotor('A');
                         brick.StopMotor('B');
                         
                         distance = brick.UltrasonicDist(3);
+                        disp(distance);
                         
 
-                        
                         if (distance < 15)
                             
                             % turn backwards (around 3pi/2)
                             
                             brick.MoveMotor('A', 27);
-                            brick.MoveMotor('B', -29);
+                            brick.MoveMotor('B', -28.785);
                             pause(0.875);
                             brick.StopMotor('A');
                             brick.StopMotor('B');
-                            startMoving = 0;
+                           
                             
                             distance = brick.UltrasonicDist(3);
+                            startMoving = 1;
                             
                             
                         end
