6c4ae403d2c1e392bf1be77ef41279e052646461
Markay12 <54555076+Markay12@users.noreply.github.com>
2019-12-03 23:20:08 -0700

    Working touch switch sensor for both 1 and 2

--- touchSwitch/BeepSwitch.m
+++ touchSwitch/BeepSwitch.m
@@ -2,7 +2,8 @@ disp('PUSH the button to start the tone');
 
 while 1
     touch = brick.TouchPressed(1);
-    if touch
+    touch2 = brick.TouchPressed(2);
+    if touch || touch2
         brick.beep();
         break;
     end
